30c060dd9960ce93e729840167148e6db91a18b2
keembogeul <318qhtmf@naver.com>
2023-07-30 16:49:57 +0900

    next.js 연습

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import fetch from 'isomorphic-unfetch';
+
+const index = ({ user }) => {
+  const username = user && user.name;
+  return <div>{username}</div>;
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const res = await fetch('https://api.github.com/users/jerrynim');
+    if (res.status === 200) {
+      const user = await res.json();
+      return { props: { user } };
+    }
+    return { props: {} };
+  } catch (e) {
+    console.log(e);
+    return { props: {} };
+  }
+};
+
+export default index;
---
+++ pages/vegetable/[name].jsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const name = () => {
+  const { query } = useRouter();
+  return (
+    <div>
+      <h2>Hello! {query.name}</h2>
+      <Link href="/" legacyBehavior>
+        Move to '/'
+      </Link>
+    </div>
+  );
+};
+
+export default name;
